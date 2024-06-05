984f562391465af8abb367627c55aca2642e85fc
Davide Andreoli <davide.andreoli@icloud.com>
2024-06-05 16:06:26 +0200

    not working

---
+++ LibretroBeetleGBA/LibretroBeetleGBA.h
@@ -0,0 +1,18 @@
+//
+//  LibretroBeetleGBA.h
+//  LibretroBeetleGBA
+//
+//  Created by Davide Andreoli on 05/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibretroBeetleGBA.
+FOUNDATION_EXPORT double LibretroBeetleGBAVersionNumber;
+
+//! Project version string for LibretroBeetleGBA.
+FOUNDATION_EXPORT const unsigned char LibretroBeetleGBAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibretroBeetleGBA/PublicHeader.h>
+
+#import <LibretroBeetleGBA/libretro.h>
