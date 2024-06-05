ddedad2f7ab13b32ff58637bea8568d3eb5b62c6
Davide Andreoli <davide.andreoli@icloud.com>
2024-06-05 08:48:04 +0200

    Initial Commit

---
+++ ArcadiaGBACore/ArcadiaGBACore.h
@@ -0,0 +1,18 @@
+//
+//  ArcadiaGBACore.h
+//  ArcadiaGBACore
+//
+//  Created by Davide Andreoli on 05/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArcadiaGBACore.
+FOUNDATION_EXPORT double ArcadiaGBACoreVersionNumber;
+
+//! Project version string for ArcadiaGBACore.
+FOUNDATION_EXPORT const unsigned char ArcadiaGBACoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArcadiaGBACore/PublicHeader.h>
+
+
