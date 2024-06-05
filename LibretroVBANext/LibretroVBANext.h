058e2687059f5ab0a466025ce3efa6614d8fd4b1
Davide Andreoli <davide.andreoli@icloud.com>
2024-06-05 18:11:02 +0200

    working

---
+++ LibretroVBANext/LibretroVBANext.h
@@ -0,0 +1,18 @@
+//
+//  LibretroVBANext.h
+//  LibretroVBANext
+//
+//  Created by Davide Andreoli on 05/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibretroVBANext.
+FOUNDATION_EXPORT double LibretroVBANextVersionNumber;
+
+//! Project version string for LibretroVBANext.
+FOUNDATION_EXPORT const unsigned char LibretroVBANextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibretroVBANext/PublicHeader.h>
+
+#import <LibretroVBANext/libretro.h>
